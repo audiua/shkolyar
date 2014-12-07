1cdeb57c04fbdcf4dbd207603f02c6198efb0ae9
audiua <audiua@yandex.ru>
2014-12-07 22:28:26 +0200

    установка фруймворка

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
 
-	echo 'Hello World';
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../../work/yii_15/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
-?>
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
