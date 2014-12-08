b1b4499b971aadc0649d5adbebe0fde2083ecec8
audiua <audiua@yandex.ru>
2014-12-08 22:24:03 +0200

    vk group

--- themes/market/views/layouts/sidebar.php
+++ themes/market/views/layouts/sidebar.php
@@ -30,7 +30,7 @@
 <!-- VK Widget -->
 <div id="vk_groups"></div>
 <script type="text/javascript">
-VK.Widgets.Group("vk_groups", {mode: 0, width: "300", height: "300", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 81422422);
+VK.Widgets.Group("vk_groups", {mode: 0, width: "300", height: "200", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 81422422);
 </script>
 </div>
 
