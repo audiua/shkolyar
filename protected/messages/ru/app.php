8711aef790edcd1c1a65ae0c84f2991f85fbc309
audiua <audiua@yandex.ru>
2014-12-08 11:24:36 +0200

    message ru

---
+++ protected/messages/ru/app.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'submit'=>'Отправить',
+	'Home'=>'Главная',
+	'About'=>'О нас',
+	'Contact'=>'Контакты',
+	'Login'=>'Войти',
+	'Logout'=>'Выйти',
+	'Operations'=>'Операции',
+	'Menu'=>'',
+	'ID'=>'',
+	'Category'=>'Категории',
+	'Title'=>'',
+	'Position'=>'',
+	'Text'=>'',
+	'Content'=>'',
+	'Content'=>'',
+	'Name'=>'',
+	'Password'=>'',
+	'Update GdzBook'=>'Обновить сборник гдз',
+	'Update TextbookBook'=>'Обновить учебник',
+];
