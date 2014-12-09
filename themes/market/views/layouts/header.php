3929a6dae251dcff1c51722db9bb4d3cdaa85c98
audiua <audiua@yandex.ru>
2014-12-09 13:52:49 +0200

    google search

--- themes/market/views/layouts/header.php
+++ themes/market/views/layouts/header.php
@@ -340,13 +340,29 @@
 
 <!-- <div class="search"> -->
 	<form action="/site/search" class="search">
+
+		<script>
+		  (function() {
+		    var cx = '006022602701960109764:drx4nhjaekq';
+		    var gcse = document.createElement('script');
+		    gcse.type = 'text/javascript';
+		    gcse.async = true;
+		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		        '//www.google.com/cse/cse.js?cx=' + cx;
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(gcse, s);
+		  })();
+		</script>
+		<gcse:searchbox-only></gcse:searchbox-only>
+
+
 		
 		<!-- <div class="show-filters"><span class="blue glyphicon glyphicon-chevron-left"></span></div> -->
 		<!-- <input type="text" name="c" value="" class="search-clas" placeholder="Клас" >
 		<input type="text" name="s" value="" class="search-subject" placeholder="Предмет" > -->
-		<input type="text" name="a" value="" class="search-text" placeholder="Текст пошуку" >
+		<!-- <input type="text" name="a" value="" class="search-text" placeholder="Текст пошуку" > -->
 		
-		<button class="search-btn">Пошук</button>
+		<!-- <button class="search-btn">Пошук</button> -->
 
 	</form>
 <!-- </div> -->
