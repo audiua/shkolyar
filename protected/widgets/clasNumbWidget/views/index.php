a80f30274b16fce8cbd4b95d0e4288b0136b8acb
audiua <audiua@yandex.ru>
2015-01-25 21:55:46 +0200

    fix

--- protected/widgets/clasNumbWidget/views/index.php
+++ protected/widgets/clasNumbWidget/views/index.php
@@ -12,7 +12,7 @@
 ?>
 
 	<div class="clas-number <?php echo $cl = ($count - 1) == $i ? 'last-clas-number' : '' ; ?> ">
-		<h3><a class="clas-<?php echo $one->clas->slug; ?>" href="<?php echo '/'.$this->controller->id.'/'. $url ; ?>" title="<?php echo $contr .' '. $one->clas->slug; ?> кдас"><?php echo $one->clas->slug; ?></a></h3>
+		<h3><a class="clas-<?php echo $one->clas->slug; ?>" href="<?php echo '/'.$this->controller->id.'/'. $url ; ?>" title="<?php echo $contr .' '. $one->clas->slug; ?> клас"><?php echo $one->clas->slug; ?></a></h3>
 	</div>
 
 <?php endif;  endforeach; ?>
