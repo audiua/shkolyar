8b5d5446b67016078f623304895d75db09192ec9
audiua <audiua@yandex.ru>
2015-02-03 10:27:48 +0200

    ref banner

--- themes/market/views/layouts/sidebar.php
+++ themes/market/views/layouts/sidebar.php
@@ -1,8 +1,60 @@
-<!-- <div class="sidebar-block">
-	<div class="direct">
-		<img class='big_adsance' src="/images/big.jpeg"> 
+<div class="sidebar-block ref">
+	<object id="textru-flash-1" type="application/x-shockwave-flash" data="http://text.ru/images/partner/banner-1.swf" width="316" height="381">
+		<param name="quality" value="high">
+	    <param name="wmode" value="transparent">
+		<p>Баннер text.ru</p>
+	</object>
+	<script type='text/javascript'>
+		var flash = document.getElementById("textru-flash-1");
+		var linkParent = flash.parentNode;
+		var innerForm = document.createElement("form");
+		innerForm.id = "textru-form-home";
+		innerForm.action = "http://text.ru/";
+		innerForm.method = "post";
+		innerForm.target = "_blank";
+		innerForm.style.display = "none";
+		var innerInput = document.createElement("input");
+		innerInput.type = "hidden";
+		innerInput.value = "audiua";
+		innerInput.name = "ref";
+		innerForm.appendChild(innerInput);
+		linkParent.appendChild(innerForm);
+		flash.addEventListener('mousedown', function() {
+			document.getElementById("textru-form-home").submit();
+			return false;
+		});
+	</script>
+</div>
+
+<div class="separator"></div>
+<div class="clear"></div>
+
+<div class="sidebar-block">
+	<div class="menu">
+		<div class="info">Навігація</div>
+
+		<?php if( $this->id == 'gdz' || $this->id == 'textbook' ):  ?>
+			<?php $this->widget('BookSidebarMenuWidget'); ?>
+		<?php elseif($this->id == 'knowall') : ?>
+			<?php $this->widget('KnowallSidebarMenuWidget'); ?>
+		<?php elseif($this->id == 'library') : ?>
+			<?php $this->widget('LibrarySidebarMenuWidget'); ?>
+		<?php elseif($this->id == 'writing') : ?>
+			<?php $this->widget('WritingSidebarMenuWidget'); ?>
+		<?php else: ?>
+			<?php $this->widget('SidebarMenuWidget'); ?>
+		<?php endif; ?>
+		
 	</div>
-</div> -->
+
+	<!-- <img src="/images/b_l.png">  -->
+
+	
+</div>
+
+<div class="separator"></div>
+<div class="clear"></div>
+
 
 <div class="sidebar-block">
 	<div class="info">Соц. мережі</div>
@@ -65,32 +117,6 @@
 		</div>
 	</noindex>
 
-</div>
-
-<div class="separator"></div>
-<div class="clear"></div>
-
-<div class="sidebar-block">
-	<div class="menu">
-		<div class="info">Навігація</div>
-
-		<?php if( $this->id == 'gdz' || $this->id == 'textbook' ):  ?>
-			<?php $this->widget('BookSidebarMenuWidget'); ?>
-		<?php elseif($this->id == 'knowall') : ?>
-			<?php $this->widget('KnowallSidebarMenuWidget'); ?>
-		<?php elseif($this->id == 'library') : ?>
-			<?php $this->widget('LibrarySidebarMenuWidget'); ?>
-		<?php elseif($this->id == 'writing') : ?>
-			<?php $this->widget('WritingSidebarMenuWidget'); ?>
-		<?php else: ?>
-			<?php $this->widget('SidebarMenuWidget'); ?>
-		<?php endif; ?>
-		
-	</div>
-
-	<!-- <img src="/images/b_l.png">  -->
-
-	
 </div>
 
 <div class="separator"></div>
