7a56a115f917db049d1f54dc99ad121d4a85c9ec
audiua <audiua@yandex.ru>
2015-02-04 12:45:35 +0200

    not responsive

--- themes/market/views/layouts/main.php
+++ themes/market/views/layouts/main.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="<?php echo CHtml::encode($this->description); ?>" />
         <meta name="keywords" content="<?php echo CHtml::encode($this->keywords); ?>" />
         <base href="<?php  echo Yii::app()->createAbsoluteUrl('/'); ?>">
