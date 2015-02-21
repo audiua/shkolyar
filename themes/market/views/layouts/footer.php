810b1c3048b6c403544833e92ba7f07b163177de
audiua <audiua@yandex.ru>
2015-02-21 10:48:21 +0200

    fix

--- themes/market/views/layouts/footer.php
+++ themes/market/views/layouts/footer.php
@@ -42,18 +42,7 @@
       	</div>
     	
     	<div class="vk-like-widget">
-	    	<!-- Put this script tag to the <head> of your page -->
-			<script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
-
-			<script type="text/javascript">
-			  VK.init({apiId: 4750024, onlyWidgets: true});
-			</script>
-
-			<!-- Put this div tag to the place, where the Like block will be -->
-			<div id="vk_like"></div>
-			<script type="text/javascript">
-			VK.Widgets.Like("vk_like", {type: "button"});
-			</script>
+	    	
     	</div>
 
 		<div class="clear"></div>
