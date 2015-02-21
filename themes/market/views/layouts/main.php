51abdc7edce90fb61421a82e64f245bbd6e08297
audiua <audiua@yandex.ru>
2015-02-21 23:16:56 +0200

    fix

--- themes/market/views/layouts/main.php
+++ themes/market/views/layouts/main.php
@@ -50,10 +50,6 @@
 
         <!-- Start Facebook Opengraph -->
         <meta property="fb:app_id" content="919200554758445">
-        <meta property="og:type" content="article">
-        <meta property="og:title" content="школяр">
-        <meta property="og:site_name" content="школяр">
-        <meta property="og:description" content="школяр">
         <meta property="og:url" content="http://shkolyar.info">
         <!-- <meta property="og:image" content="<зображення запису>"/> -->
         <!-- End Facebook Opengraph -->
