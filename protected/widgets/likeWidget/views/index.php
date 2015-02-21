59322e7fb336a29828a57f5cbda13c7527a56eaf
audiua <audiua@yandex.ru>
2015-02-21 21:39:31 +0200

    social btn

--- protected/widgets/likeWidget/views/index.php
+++ protected/widgets/likeWidget/views/index.php
@@ -6,12 +6,26 @@
 	  VK.init({apiId: 4750024, onlyWidgets: true});
 	</script>
 
+	<!-- VK Widget -->
+	<div class="vk-subscribe">
+		<div id="vk_subscribe"></div>
+		<script type="text/javascript">
+		VK.Widgets.Subscribe("vk_subscribe", {soft: 1}, -81422422);
+		</script>
+	</div>
+	
 	<!-- Put this div tag to the place, where the Like block will be -->
-	<div id="vk_like"></div>
-	<script type="text/javascript">
-	VK.Widgets.Like("vk_like", {type: "button"});
-	</script>
+	<div class="vk-like">
+		<div id="vk_like"></div>
+		<script type="text/javascript">
+		VK.Widgets.Like("vk_like", {type: "button"});
+		</script>
+	</div>
 
-	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fshkolyar.info.page&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=1565122747059411" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+	<div class="fb-like">
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fshkolyar.info.page&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=1565122747059411" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+	</div>
+
+
 
 </div>
