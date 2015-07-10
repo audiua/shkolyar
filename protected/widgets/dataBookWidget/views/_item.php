f128127096e1ac186e25af4ede2c2f581d5442ce
audiua <audiua@yandex.ru>
2015-07-10 22:10:15 +0300

    fix

--- protected/widgets/dataBookWidget/views/_item.php
+++ protected/widgets/dataBookWidget/views/_item.php
@@ -11,9 +11,8 @@ $contr = ($this->controller->id=='gdz') ? 'ГДЗ ':'Підручник ';
 echo CHtml::image(Yii::app()->baseUrl . '/' . $path.'/'.$data->slug.'.'.$data->img, 'SHKOLYAR.INFO - ' . $contr . $data->$categoryClas->clas->slug . ' клас ' . $data->$categorySubject->subject->title . ' ' .  $data->author, array('class'=>'img-middle-book thumbnail ', 'title'=>'SHKOLYAR.INFO - ' . $contr . $data->$categoryClas->clas->slug . ' клас ' . $data->$categorySubject->subject->title . ' ' .  $data->author)) ?> </div>
 	<div class="">
 
-		<?php echo CHtml::link( 'ГДЗ '. $data->gdz_clas->clas->slug . ' клас '. $data->gdz_subject->subject->title . ' ' .$data->author, array('/gdz/'.$data->$categoryClas->clas->slug.'/'.$data->$categorySubject->subject->slug.'/'.$data->slug)); ?>
+		<?php echo CHtml::link( 'ГДЗ '. $data->$categoryClas->clas->slug . ' клас '. $data->$categorySubject->subject->title . ' ' .$data->author, array('/gdz/'.$data->$categoryClas->clas->slug.'/'.$data->$categorySubject->subject->slug.'/'.$data->slug)); ?>
 
-		
 		<?php 
 			if( !empty($data->properties) ){
 				echo '<div class="desc">'.$data->properties.'</div>';
