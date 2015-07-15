42eea28fe1a56066cf4830ee3075935f5a792fb9
audiua <audiua@yandex.ru>
2015-07-15 09:09:54 +0300

    fix

--- themes/market/views/layouts/footer.php
+++ themes/market/views/layouts/footer.php
@@ -20,7 +20,13 @@
 		<li><?php  echo CHtml::link('Контакти', array('/contacts'), array('rel'=>'nofollow', 'class'=>Yii::app()->request->requestUri=='/contacts'?'active':'')); ?></li>
 		<li><?php  echo CHtml::link('Карта сайта', array('/sitemap'), array('rel'=>'nofollow', 'target'=>'_blank')); ?></li>
     <li><?php  echo CHtml::link('sitemap.xml', array('/sitemap.xml'), array('rel'=>'nofollow', 'target'=>'_blank')); ?></li>
-	 <?php $this->renderDynamic('getInsideLink'); ?>	
+	 <li><!-- Yandex.Metrika informer -->
+<a href="https://metrika.yandex.ru/stat/?id=31373293&amp;from=informer"
+target="_blank" rel="nofollow"><img src="https://mc.yandex.ru/informer/31373293/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+<!-- /Yandex.Metrika informer --></li>
+
+   <?php $this->renderDynamic('getInsideLink'); ?>	
 	</ul>
 </div>
 <?php else: ?>
