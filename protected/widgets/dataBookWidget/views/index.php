0e341703b68318ac2058587e1ca75e3e65ee674d
audiua <audiua@yandex.ru>
2015-09-05 10:32:57 +0300

    fix

--- protected/widgets/dataBookWidget/views/index.php
+++ protected/widgets/dataBookWidget/views/index.php
@@ -3,11 +3,11 @@
 
 <div class="book-list">
 
-<div class="view-filters">
+<!-- <div class="view-filters">
 	<span class="view-filter glyphicon glyphicon-th-large gray active-filter" data-view='middle-book-block'></span> 
 	<span class="view-filter glyphicon glyphicon-th gray " data-view='small-book-block'></span>  
 	<span class="view-filter glyphicon glyphicon-th-list gray" data-view='list-book-block'></span> 
-</div>
+</div> -->
 <div class="clear"></div>
 <?php
 
@@ -17,7 +17,7 @@ $this->widget('zii.widgets.CListView', array(
     'dataProvider'=>$dataProvider,
     'itemView'=>'_item',
     'summaryText' => '',
-    'sorterHeader' => 'Сортувати по: ',
+    // 'sorterHeader' => 'Сортувати по: ',
  //    'pager'=>array(
  //    	'header' => '',
  //    	'firstPageLabel'=>'<<',
