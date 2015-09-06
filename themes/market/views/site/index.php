47ae80d888cde71930dc4e95101b4af55e7d55a0
audiua <audiua@yandex.ru>
2015-09-06 14:28:55 +0300

    fix main page

--- themes/market/views/site/index.php
+++ themes/market/views/site/index.php
@@ -16,26 +16,26 @@ $this->widget('zii.widgets.CBreadcrumbs', array(
 	<?php $this->widget('DescriptionWidget', array('params'=>array('owner'=>'site', 'action'=>'index'))); ?>
 </div>
 
-<h3 class="info">ГДЗ</h3>
+<h3 class="info"><?= SeoHide::link('/gdz', 'ГДЗ'); ?></h3>
 <?php $this->widget('LastBookWidget', array('mode'=>'gdz')); ?>
 <div class="separator"></div>
 <?php $this->widget('BannerWidget', array('params'=>array('name'=>'full-banner-content-middle'))); ?>
 
 <div class="separator"></div>
-<h3 class="info">Твори</h3>
+<h3 class="info"><?= SeoHide::link('/writing', 'Твори'); ?></h3>
 <?php $this->widget('LastWritingWidget'); ?>
 <div class="separator"></div>
 
-<h3 class="info">Підручники</h3>
+<h3 class="info"><?= SeoHide::link('/textbook', 'Підручники'); ?></h3>
 <?php $this->widget('LastBookWidget', array('mode'=>'textbook')); ?>
 <div class="separator"></div>
 
-<h3 class="info">Художня література</h3>
+<h3 class="info"><?= SeoHide::link('/library', 'Художня література'); ?></h3>
 <?php $this->widget('LastLibraryWidget'); ?>
 <div class="separator"></div>
 
 <!-- <div class="info">Всезнайка</div> -->
-<h3 class="info">Всезнайка</h3>
+<h3 class="info"><?= SeoHide::link('/knowall', 'Всезнайка'); ?></h3>
 <?php $this->widget('LastKnowallWidget'); ?>
 <div class="separator"></div>
 
