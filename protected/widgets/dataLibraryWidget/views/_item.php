cced09878cf5e8192774520c64486ac7071384b3
audiua <audiua@yandex.ru>
2015-09-06 14:46:21 +0300

    fix

--- protected/widgets/dataLibraryWidget/views/_item.php
+++ protected/widgets/dataLibraryWidget/views/_item.php
@@ -2,18 +2,16 @@
 
 <div class="middle-book-block">
 
-<div class=""> <?php echo CHtml::image(Yii::app()->baseUrl . '/' . $path.'/first.'.$data->img_ext, 'SHKOLYAR.INFO - Художня література ' . $data->library_author->author . ' ’’'.$data->title.'’’', array('class'=>'img-middle-book thumbnail ', 'title'=>'SHKOLYAR.INFO - Художня література ' . $data->library_author->author . ' ’’'.$data->title.'’’')) ?> </div>
+<div class=""> <?php 
+echo SeoHide::link('/library/'.$data->library_author->slug.'/'.$data->slug, CHtml::image(Yii::app()->baseUrl . '/' . $path.'/first.'.$data->img_ext, $data->library_author->author . ' ’’'.$data->title.'’’', array('class'=>'img-middle-book thumbnail ')));?>
+</div>
 	<div class="">
-		<div class="book-author"> <?php echo $data->library_author->author; ?></div>
-		<div class="book-subject"> <?php echo $data->title; ?></div>
-		<div class="desc">
-			<!-- <p><?php // echo $data->description; 
-			?></p> -->
-		</div>
+	<?php echo CHtml::link('
+		<div class="book-author">'.$data->library_author->author.'</div>
+		<div class="book-subject">'.$data->title.'</div>', 
+		array('/library/'.$data->library_author->slug.'/'.$data->slug));?>
 
 	</div>
 
-	<div class="gdz-link">
-		<?php echo CHtml::link( 'Перейти', array('/library/'.$data->library_author->slug.'/'.$data->slug), array('class'=>'btn btn-primary btn-sm')); ?>
-	</div>		
+		
 </div>
