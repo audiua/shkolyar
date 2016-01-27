40ce8360d673c0090e003a876d5a3af0f0dfe42c
audiua <audiua@yandex.ru>
2016-01-27 12:29:22 +0200

    fix

--- protected/widgets/textbookSidebarMenuWidget/TextbookSidebarMenuWidget.php
+++ protected/widgets/textbookSidebarMenuWidget/TextbookSidebarMenuWidget.php
@@ -23,21 +23,28 @@ class TextbookSidebarMenuWidget extends CWidget{
 			$subject = null;
 		}
 
-		$criteria = new CDbCriteria;
-		$criteria->condition = 't.textbook_clas_id='.$this->controller->clasModel->id;
-		$criteria->addCondition('t.public=1');
-		$criteria->addCondition('t.public_time<'.time());
-		$criteria->group = 'textbook_subject_id';
+		if($clas){
 
-		$subjectsIds = TextbookBook::model()->findAll($criteria);
-		foreach($subjectsIds as $s){
-			$ids[] = $s->textbook_subject_id;
+			$criteria = new CDbCriteria;
+			$criteria->condition = 't.textbook_clas_id='.$this->controller->clasModel->id;
+			$criteria->addCondition('t.public=1');
+			$criteria->addCondition('t.public_time<'.time());
+			$criteria->group = 'textbook_subject_id';
+
+			$subjectsIds = TextbookBook::model()->findAll($criteria);
+			foreach($subjectsIds as $s){
+				$ids[] = $s->textbook_subject_id;
+			}
+
+			$criteria = new CDbCriteria;
+			$criteria->addInCondition('t.id', $ids);
+			
+			$subjects = TSubject::model()->findAll($criteria);
+		} else {
+			$subjects = array();
+
 		}
 
-		$criteria = new CDbCriteria;
-		$criteria->addInCondition('t.id', $ids);
-		
-		$subjects = TSubject::model()->findAll($criteria);
 
        // передаем данные в представление виджета
        $this->render('index', array('clas'=>$clas,'subject'=>$subject, 'allSubjectModel'=>$subjects));
